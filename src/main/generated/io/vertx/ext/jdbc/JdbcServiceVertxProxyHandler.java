58aee57fab8f39dd81d5f62e5f9ed8f76546e404
Julien Viet <julien@julienviet.com>
2015-04-05 18:16:04 +0200

    Releasing 3.0.0-milestone4

--- src/main/generated/io/vertx/ext/jdbc/JdbcServiceVertxProxyHandler.java
+++ src/main/generated/io/vertx/ext/jdbc/JdbcServiceVertxProxyHandler.java
@@ -44,11 +44,44 @@ public class JdbcServiceVertxProxyHandler extends ProxyHandler {
   private final Vertx vertx;
   private final JdbcService service;
   private final String address;
+  private final long timerID;
+  private long lastAccessed;
+  private final long timeoutSeconds;
 
-  public JdbcServiceVertxProxyHandler(Vertx vertx, JdbcService service, String address) {
+  public JdbcServiceVertxProxyHandler(Vertx vertx, JdbcService service, String address, boolean topLevel, long timeoutSeconds) {
     this.vertx = vertx;
     this.service = service;
     this.address = address;
+    this.timeoutSeconds = timeoutSeconds;
+    if (timeoutSeconds != -1 && !topLevel) {
+      long period = timeoutSeconds * 1000 / 2;
+      if (period > 10000) {
+        period = 10000;
+      }
+      this.timerID = vertx.setPeriodic(period, this::checkTimedOut);
+    } else {
+      this.timerID = -1;
+    }
+    accessed();
+  }
+
+  private void checkTimedOut(long id) {
+    long now = System.nanoTime();
+    if (now - lastAccessed > timeoutSeconds * 1000000000) {
+      close();
+    }
+  }
+
+  @Override
+  public void close() {
+    if (timerID != -1) {
+      vertx.cancelTimer(timerID);
+    }
+    super.close();
+  }
+
+  private void accessed() {
+    this.lastAccessed = System.nanoTime();
   }
 
   public void handle(Message<JsonObject> msg) {
@@ -57,6 +90,7 @@ public class JdbcServiceVertxProxyHandler extends ProxyHandler {
     if (action == null) {
       throw new IllegalStateException("action not specified");
     }
+    accessed();
     switch (action) {
 
 
@@ -66,7 +100,7 @@ public class JdbcServiceVertxProxyHandler extends ProxyHandler {
             msg.fail(-1, res.cause().getMessage());
           } else {
             String proxyAddress = UUID.randomUUID().toString();
-            ProxyHelper.registerService(SqlConnection.class, vertx, res.result(), proxyAddress);
+            ProxyHelper.registerService(SqlConnection.class, vertx, res.result(), proxyAddress, false, timeoutSeconds);
             msg.reply(null, new DeliveryOptions().addHeader("proxyaddr", proxyAddress));
           }
         });
