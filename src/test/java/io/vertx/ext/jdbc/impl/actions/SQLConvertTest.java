1d4d0b742866ed5aa10300b13ce2879ed3c1fbd5
Thomas Segismont <tsegismo@redhat.com>
2016-12-15 11:24:35 +0100

    Check JDBC type

--- src/test/java/io/vertx/ext/jdbc/impl/actions/SQLConvertTest.java
+++ src/test/java/io/vertx/ext/jdbc/impl/actions/SQLConvertTest.java
@@ -12,6 +12,7 @@ import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
+import java.util.UUID;
 
 import static java.time.format.DateTimeFormatter.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -31,27 +32,28 @@ public class SQLConvertTest {
     ZonedDateTime dateTime = ZonedDateTime.of(2016, 3, 16, 16, 0, 0, 0, ZoneId.of("Europe/Paris"));
     for (int i = 0; i < 4; i++) {
       int nanos = 123 * (i == 0 ? 0 : 1) * (int) Math.pow(1000, i > 1 ? i - 1 : 0);
-      params.add(new Object[]{ISO_INSTANT.format(dateTime.withNano(nanos))});
+      params.add(new Object[]{ISO_INSTANT.format(dateTime.withNano(nanos)), java.sql.Timestamp.class});
     }
-    params.add(new Object[]{ISO_LOCAL_TIME.format(dateTime.withSecond(1))});
-    params.add(new Object[]{dateTime.toLocalDate().toString()});
+    params.add(new Object[]{ISO_LOCAL_TIME.format(dateTime.withSecond(1)), java.sql.Time.class});
+    params.add(new Object[]{dateTime.toLocalDate().toString(), java.sql.Date.class});
 
-    params.add(new Object[]{"f47ac10b-58cc-4372-a567-0e02b2c3d479"});
+    params.add(new Object[]{"f47ac10b-58cc-4372-a567-0e02b2c3d479", UUID.class});
 
     return params;
   }
 
-  // Fields
   private String value;
+  private Class<?> expectedSqlType;
 
-  public SQLConvertTest(String value) {
+  public SQLConvertTest(String value, Class<?> expectedSqlType) {
     this.value = value;
+    this.expectedSqlType = expectedSqlType;
   }
 
   @Test
   public void testSQLConvert() throws SQLException {
     Object cast = helper.optimisticCast(value);
-    assertThat(cast, not(instanceOf(String.class)));
+    assertThat(cast, instanceOf(expectedSqlType));
 
     Object convert = JDBCStatementHelper.convertSqlValue(cast);
     assertEquals(value, convert);
