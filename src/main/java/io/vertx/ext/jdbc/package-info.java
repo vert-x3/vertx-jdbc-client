c19d585b5af38ef9a155cc641aa0ecafaa424654
Paulo Lopes <paulo@mlopes.net>
2018-03-23 10:39:35 +0100

    Fix module name mismatch

--- src/main/java/io/vertx/ext/jdbc/package-info.java
+++ src/main/java/io/vertx/ext/jdbc/package-info.java
@@ -13,7 +13,7 @@
  *
  * You may elect to redistribute this code under either of these licenses.
  */
-@ModuleGen(name = "vertx-jdbc", groupPackage = "io.vertx")
+@ModuleGen(name = "vertx-jdbc-client", groupPackage = "io.vertx")
 package io.vertx.ext.jdbc;
 
 import io.vertx.codegen.annotations.ModuleGen;
