b9006f99bda784ebb1d60c19ad75201d99b68fe3
Ruslan Sennov <ruslan.sennov@gmail.com>
2020-04-21 14:38:22 +0300

    more optimisticCast options (#183)
    Signed-off-by: Ruslan Sennov <ruslan.sennov@gmail.com>

--- src/test/java/io/vertx/ext/jdbc/impl/actions/OptimisticCastTest.java
+++ src/test/java/io/vertx/ext/jdbc/impl/actions/OptimisticCastTest.java
@@ -13,20 +13,24 @@ import static org.junit.Assert.*;
 @RunWith(Parameterized.class)
 public class OptimisticCastTest {
 
-  private JDBCStatementHelper helper = new JDBCStatementHelper(new JsonObject().put("castUUID", true));
+  private JDBCStatementHelper helper;
 
   @Parameterized.Parameters
   public static Collection<Object[]> generateData() {
     return Arrays.asList(new Object[][]{
-        // simple types
-        {"16:00:00", "java.sql.Time"},
-        {"2016-03-16", "java.sql.Date"},
-        {"2016-03-16T16:00:00Z", "java.sql.Timestamp"},
-        {"f47ac10b-58cc-4372-a567-0e02b2c3d479", "java.util.UUID"},
-        // bad variations
-        {"2016-03-16T16:00:00", "java.lang.String"},
-        {"24:00:00", "java.lang.String"},
-        {"2016-00-00", "java.lang.String"},
+      // simple types
+      {"16:00:00", "java.sql.Time", null},
+      {"16:00:00", "java.lang.String", new JsonObject().put("castTime", false)},
+      {"2016-03-16", "java.sql.Date", null},
+      {"2016-03-16", "java.lang.String", new JsonObject().put("castDate", false)},
+      {"2016-03-16T16:00:00Z", "java.sql.Timestamp", null},
+      {"2016-03-16T16:00:00Z", "java.lang.String", new JsonObject().put("castDatetime", false)},
+      {"f47ac10b-58cc-4372-a567-0e02b2c3d479", "java.util.UUID", new JsonObject().put("castUUID", true)},
+      {"f47ac10b-58cc-4372-a567-0e02b2c3d479", "java.lang.String", null},
+      // bad variations
+      {"2016-03-16T16:00:00", "java.lang.String", null},
+      {"24:00:00", "java.lang.String", null},
+      {"2016-00-00", "java.lang.String", null},
     });
   }
 
@@ -34,7 +38,8 @@ public class OptimisticCastTest {
   private String value;
   private String expectedType;
 
-  public OptimisticCastTest(String value, String expectedType) {
+  public OptimisticCastTest(String value, String expectedType, JsonObject config) {
+    this.helper = config == null ? new JDBCStatementHelper() : new JDBCStatementHelper(config);
     this.value = value;
     this.expectedType = expectedType;
   }
