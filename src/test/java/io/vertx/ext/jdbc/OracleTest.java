a998153ad18862661d8654d4a59cda109739ec8b
Paulo Lopes <pmlopes@gmail.com>
2021-06-15 12:26:27 +0200

    Fix #169: Please upgrade to the new sql-client API
    Signed-off-by: Paulo Lopes <pmlopes@gmail.com>

--- src/test/java/io/vertx/ext/jdbc/OracleTest.java
+++ src/test/java/io/vertx/ext/jdbc/OracleTest.java
@@ -39,4 +39,39 @@ public class OracleTest {
         test.complete();
       });
   }
+
+  @Test
+  @Ignore("Cannot run this in CI as we can't install Oracle")
+  public void testBlocking(TestContext should) {
+//    CREATE OR REPLACE FUNCTION MYSCHEMA.TEST_SLEEP
+//      (
+//        TIME_  IN  NUMBER
+//      )
+//    RETURN INTEGER IS
+//      BEGIN
+//    DBMS_LOCK.sleep(seconds => TIME_);
+//    RETURN 1;
+//    EXCEPTION
+//    WHEN OTHERS THEN
+//      RAISE;
+//    RETURN 1;
+//    END TEST_SLEEP;
+
+    final Async test = should.async();
+
+    final JDBCPool pool = JDBCPool.pool(
+      rule.vertx(),
+      new JDBCConnectOptions()
+        .setJdbcUrl("jdbc:oracle:thin:@127.0.0.1:1521:xe")
+        .setUser("sys as sysdba")
+        .setPassword("vertx"),
+      new PoolOptions());
+
+    pool.preparedQuery("SELECT TEST_SLEEP(10.5) FROM DUAL")
+      .execute()
+      .onFailure(should::fail)
+      .onSuccess(rowSet -> {
+        test.complete();
+      });
+  }
 }
