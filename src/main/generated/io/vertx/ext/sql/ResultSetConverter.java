6add66d3bb3ff1a8d3094c3e80719f9ed0732aff
Julien Viet <julien@julienviet.com>
2023-12-13 16:59:04 +0100

    Set version to 4.5.2-SNAPSHOT

--- src/main/generated/io/vertx/ext/sql/ResultSetConverter.java
+++ src/main/generated/io/vertx/ext/sql/ResultSetConverter.java
@@ -17,7 +17,7 @@ public class ResultSetConverter {
   private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
   private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
 
-  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ResultSet obj) {
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, ResultSet obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
       switch (member.getKey()) {
         case "columnNames":
@@ -62,11 +62,11 @@ public class ResultSetConverter {
     }
   }
 
-  public static void toJson(ResultSet obj, JsonObject json) {
+   static void toJson(ResultSet obj, JsonObject json) {
     toJson(obj, json.getMap());
   }
 
-  public static void toJson(ResultSet obj, java.util.Map<String, Object> json) {
+   static void toJson(ResultSet obj, java.util.Map<String, Object> json) {
     if (obj.getColumnNames() != null) {
       JsonArray array = new JsonArray();
       obj.getColumnNames().forEach(item -> array.add(item));
--- src/main/generated/io/vertx/ext/sql/UpdateResultConverter.java
+++ src/main/generated/io/vertx/ext/sql/UpdateResultConverter.java
@@ -17,7 +17,7 @@ public class UpdateResultConverter {
   private static final Base64.Decoder BASE64_DECODER = JsonUtil.BASE64_DECODER;
   private static final Base64.Encoder BASE64_ENCODER = JsonUtil.BASE64_ENCODER;
 
-  public static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, UpdateResult obj) {
+   static void fromJson(Iterable<java.util.Map.Entry<String, Object>> json, UpdateResult obj) {
     for (java.util.Map.Entry<String, Object> member : json) {
       switch (member.getKey()) {
         case "keys":
@@ -34,11 +34,11 @@ public class UpdateResultConverter {
     }
   }
 
-  public static void toJson(UpdateResult obj, JsonObject json) {
+   static void toJson(UpdateResult obj, JsonObject json) {
     toJson(obj, json.getMap());
   }
 
-  public static void toJson(UpdateResult obj, java.util.Map<String, Object> json) {
+   static void toJson(UpdateResult obj, java.util.Map<String, Object> json) {
     if (obj.getKeys() != null) {
       json.put("keys", obj.getKeys());
     }
