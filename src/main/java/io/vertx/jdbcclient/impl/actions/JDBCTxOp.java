4dab4a9fa41306d7d3b6181ed270f6dc42efcdf2
Thomas Segismont <tsegismont@gmail.com>
2026-07-23 22:31:53 +0200

    Reset autocommit after commit or rollback
    See #356
    
    Signed-off-by: Thomas Segismont <tsegismont@gmail.com>

--- src/main/java/io/vertx/jdbcclient/impl/actions/JDBCTxOp.java
+++ src/main/java/io/vertx/jdbcclient/impl/actions/JDBCTxOp.java
@@ -1,15 +1,15 @@
 /*
- * Copyright (c) 2011-2014 The original author or authors
- * ------------------------------------------------------
+ * Copyright (c) 2011-2026 The original author or authors
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * and Apache License v2.0 which accompanies this distribution.
  *
- *     The Eclipse Public License is available at
- *     http://www.eclipse.org/legal/epl-v10.html
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
  *
- *     The Apache License v2.0 is available at
- *     http://www.opensource.org/licenses/apache2.0.php
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
  *
  * You may elect to redistribute this code under either of these licenses.
  */
@@ -46,7 +46,7 @@ public class JDBCTxOp<R> extends AbstractJDBCAction<R> {
           conn.rollback();
         }
       } finally {
-        conn.setAutoCommit(false);
+        conn.setAutoCommit(true);
       }
     }
     return op.result();
--- src/test/resources/init-mysql.sql
+++ src/test/resources/init-mysql.sql
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2011-2026 The original author or authors
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Apache License v2.0 which accompanies this distribution.
+ *
+ *      The Eclipse Public License is available at
+ *      http://www.eclipse.org/legal/epl-v10.html
+ *
+ *      The Apache License v2.0 is available at
+ *      http://www.opensource.org/licenses/apache2.0.php
+ *
+ * You may elect to redistribute this code under either of these licenses.
+ */
+
 -- MySQL initialization script
 
 CREATE TABLE binary_data_type
@@ -19,3 +35,10 @@ PRIMARY KEY (ID));
 INSERT INTO animal (IS_PET, NAME) VALUES (TRUE, 'dog');
 INSERT INTO animal (IS_PET, NAME) VALUES (TRUE, 'cat');
 INSERT INTO animal (IS_PET, NAME) VALUES (FALSE, 'cow');
+
+CREATE TABLE people
+(
+  ID   INT          NOT NULL AUTO_INCREMENT,
+  NAME VARCHAR(100) NOT NULL,
+  PRIMARY KEY (ID)
+);
